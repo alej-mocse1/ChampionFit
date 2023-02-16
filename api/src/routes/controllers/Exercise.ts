98dfffad2f5e7cfe22c7e266ec3473b8b17fd37a
Facu-J-Boy <boyfacundo45@gmail.com>
2023-02-16 16:38:27 -0300

    exercise route put

--- api/src/routes/controllers/Exercise.ts
+++ api/src/routes/controllers/Exercise.ts
@@ -5,17 +5,31 @@ import { Exercise } from "../../models/Exercise";
 
 export const controller = {
     getAllExercise: async () => {
-        const allExercise: Promise<ExerciseDB[]> = Exercise.findAll();
+        const allExercise: ExerciseDB[] = await Exercise.findAll();
         return allExercise
     },
     getOneExercise: async (id: string) => {
-        const exercise: Promise<ExerciseDB | null> = Exercise.findOne({
+        const exercise: ExerciseDB | null = await Exercise.findOne({
             where: {id: id}
     });
     return exercise;
     },
-    postExercise: async(param: Optional<Exercise, NullishPropertiesOf<Exercise>>) => {
-        const createExercise: Promise<ExerciseDB> = Exercise.create(param);
+    postExercise: async (param: Optional<Exercise, NullishPropertiesOf<Exercise>>) => {
+        const createExercise: ExerciseDB = await Exercise.create(param);
         return createExercise;
+    },
+    putExercise: async (param: ExerciseDB) => {
+        await Exercise.update({
+            name: param.name,
+            image: param.image,
+            description: param.description,
+            time: param.time
+        },
+        {
+            where: {
+                id: param.id
+            }
+        });
+        return 'Exercise updated';
     }
 }
