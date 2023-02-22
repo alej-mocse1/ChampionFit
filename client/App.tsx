d37b0cf10f996d95fea2a00ea1d26b13f772ffd1
Facu-J-Boy <boyfacundo45@gmail.com>
2023-02-22 16:46:41 -0300

    components directory

--- client/App.tsx
+++ client/App.tsx
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>holaaa desde ale</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ client/src/components/AuthForm/authForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const authForm = () => {
+  return (
+    <View>
+        <Text>
+            authForm
+        </Text>
+    </View>
+  )
+}
+
+export default authForm
