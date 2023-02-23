07be65cc2a3e856d7f3d5b5fe28dc984ae97e983
Facu-J-Boy <boyfacundo45@gmail.com>
2023-02-23 11:02:27 -0300

    form

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  AuthForm  from './src/components/AuthForm/AuthForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <AuthForm />
       <StatusBar style="auto" />
     </View>
   );
--- client/src/components/AuthForm/authForm.tsx
+++ client/src/components/AuthForm/authForm.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-const authForm = () => {
+const AuthForm: React.FC = () => {
   return (
-    <View>
-        <Text>
-            authForm
-        </Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Email:</Text>
+      <TextInput style={styles.input} />
+
+      <Text style={styles.label}>Password:</Text>
+      <TextInput style={styles.input} />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-export default authForm
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+      },
+      label: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+      input: {
+        width: 100,
+        height: 30,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+      },
+      button: {
+        backgroundColor: 'blue',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginTop: 10,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
+  });
+
+export default AuthForm
