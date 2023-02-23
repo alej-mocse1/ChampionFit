859dc829403c5690a01a0700b25b703dcbec1c50
alej-mocse1 <alejandromcse01@gmail.com>
2023-02-23 01:48:07 -0300

    advance home in  client/src/components/home.tsx

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,35 +1,72 @@
 import React from 'react';
 import { Text, View, Image , StyleSheet } from 'react-native';
-import  authForm from './AuthForm/authForm';
 
 
-const Home = () => {
+const Home = () :JSX.Element => {
+  
   return (
-    <View >
-         <View style={styles.ConteinerView}>
-         <Image 
-          source={require('./logo.jpeg')}
-          style={styles.img}></Image>
+    <View style={styles.View1} >
+        <View  style={styles.Conteiner}>
+          <View style={styles.ConteinerView}>
+           <Image 
+            source={require('./logo.jpeg')}
+             style={styles.img}></Image>
         
-         <Text style={styles.text}>championFit</Text>
-         </View>
+            <Text style={styles.text}>championFit</Text>
+          </View>
+          <View style={styles.Viewphrase}>
+            <Text style={styles.phrase}>discipline</Text>
+           </View>
+
+          <View  style={styles.viewReady}>
+           <Text style={styles.text}>Ready?</Text>
+          </View>
+
+          <View >
+           <Text >aca van los links a las diferentes semanas</Text>
+          </View>
+
+       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  img: {
-   height:250,
-   width:250
+  View1:{
+marginTop:160
   },
-  text: {
-    fontSize: 28,
-    fontWeight: "800"
+  Conteiner:{
+    flex: 1,
+    alignItems: 'center' ,    
    },
    ConteinerView:{
     flex: 1,
     alignItems: 'center' ,
-    marginTop:-270
+    marginTop:-370
+   },
+  img: {
+   height:200,
+   width:200
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "800"
+   },
+   Viewphrase:{
+    backgroundColor: '#2B2E47',
+    height:70,
+    width:300
+   },
+   phrase:{
+    fontSize: 47,
+    fontWeight: "900",
+    color: 'white',
+    marginLeft:35
+   },
+   viewReady:{
+    marginTop:40,
+    borderBottomColor:"aqua",
+    borderBottomWidth: 7
    }
 });
 
